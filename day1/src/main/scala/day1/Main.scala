098bba058131c06c19bdfa2d57261f8875940cb0
Paul Kaifler <paul@kaifler.eu>
2023-12-02 18:14:52 +0100

    Solution day1 task 1

--- day1/src/main/scala/day1/Main.scala
+++ day1/src/main/scala/day1/Main.scala
@@ -1,5 +1,29 @@
 package day1
 
+import scala.io.Source
+
+val testFile = "day1/smallTest.txt"
+val puzzleFile = "day1/input.txt"
+def loadPuzzleInput(fromFile: String): List[String] =
+  Source.fromResource(fromFile).getLines().toList
+
+def getFirstNumber(of: String): Int =
+  val regexPattern = "[0-9]".r
+  regexPattern.findFirstIn(of) match
+    case Some(n) => n.toInt
+    case None => throw new Exception("No match")
+
 @main
 def main(): Unit =
-  println("Hello world!")
+  val puzzleLines = loadPuzzleInput(puzzleFile)
+  var accumulatedCalibration = 0
+
+  puzzleLines.foreach(line =>
+    val backwards = line.reverse
+    val firstNumber = getFirstNumber(line)
+    val lastNumber = getFirstNumber(backwards)
+
+    accumulatedCalibration += firstNumber * 10 + lastNumber
+  )
+
+  println(accumulatedCalibration)
