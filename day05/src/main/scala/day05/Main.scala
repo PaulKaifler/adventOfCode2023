fdea3e4f26c0e28201995f5bc34d0dc91bf7b1ce
Paul Kaifler <paul@kaifler.eu>
2023-12-16 12:21:34 +0100

    Logic day 5

--- day05/src/main/scala/day05/Main.scala
+++ day05/src/main/scala/day05/Main.scala
@@ -6,12 +6,53 @@ import scala.io.Source
 def loadPuzzleInput(fromFile: String): List[String] =
   Source.fromResource(fromFile).getLines().toList
 
-case class Mapping(from: Int, to: Int, interval: Int)
+case class Mapping(from: Int, to: Int, length: Int) {
+  def covers(n: Int): Boolean = n >= from && n < from + length
+
+  def translate(n: Int): Int = to + ( n - from)
+
+}
 
 case class MapGroup(startName: String, endName: String, mappings: List[Mapping]) {
   override def toString: String = s"\nfrom: $startName, to: $endName - mappings $mappings"
+
+  def translate(startNumber: Int): Int = {
+    println(s"${startName.toUpperCase()} -> ${endName.toUpperCase()}")
+    mappings.foreach {map =>
+        if map.covers(startNumber) then {
+          println(s"   Translation found:     $startNumber -> ${map.translate(startNumber)}")
+          return map.translate(startNumber)
+        }
+    }
+    println(s"   No translation cover:  $startNumber -> $startNumber")
+    startNumber
+  }
 }
 case class Almanac(seeds: List[Int], mappings: List[MapGroup]) {
+  def translate(from: String, to: String, startNumber: Int): Int = {
+
+    var translation: Int = startNumber
+    var currentName = from
+    while (!currentName.equalsIgnoreCase(to)) {
+
+      val mapGroup = mappings.find(_.startName.equalsIgnoreCase(currentName))
+      mapGroup match {
+        case Some(group) => {
+          translation = group.translate(translation)
+          currentName = group.endName
+        }
+        case None => println(s"This is not a valid start name: $from")
+      }
+    }
+    translation
+  }
+
+  def findLowestLocation(from: String): Int = {
+    val translations = seeds.map(translate(from, "location", _))
+    translations.min
+  }
+
+
   override def toString: String =
     s"""seeds:    $seeds
        |$mappings
@@ -45,7 +86,7 @@ object Almanac {
       var i = 0
       while (mapData.indices.contains(i) && !mapData(i).isBlank) {
         mapData(i) match
-          case regexThreeNumbers(from, to, interval) => maps = maps :+ Mapping(from.toInt, to.toInt, interval.toInt)
+          case regexThreeNumbers(to, from, interval) => maps = maps :+ Mapping(from.toInt, to.toInt, interval.toInt)
           case _ => throw Exception(s"Trouble parsing the three numbers ${mapData(i)}")
 
         i += 1
@@ -61,9 +102,11 @@ object Almanac {
 
 @main
 def main(): Unit = {
-  val gameFile = "day05/test.txt"
+  val gameFile = "day05/input.txt"
   val puzzleText = loadPuzzleInput(gameFile)
 
   val almanac = Almanac(puzzleText)
-  println(almanac)
+//  almanac.mappings.head.translate(13)
+//  println(almanac.translate(from = "seed", to = "location", 13))
+  println(almanac.findLowestLocation("seed"))
 }
